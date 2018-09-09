d11c45fd5dbf947089eb08a0f4be7da62e0820e2
Varun Somani <varunsomani95@gmail.com>
2018-09-09 06:24:06 +0530

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,24 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#  Input: Matrix
+#  Output: A cachematrix, which is a list containing 4 functions that:
+#  set the value of the vector
+#  get the value of the vector
+#  set the value of the mean
+#  get the value of the mean
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
+  inv <- NULL # Initialize inv as null
+  set <- function(y) { # Set Matrix function
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  get <- function() x # Get Matrix function
+  setinverse <- function(inverse) inv <<- set inverse function
+  getinverse <- function() inv # get inverse function
   list(set = set, get = get,
        setinverse = setinverse,
-       getinverse = getinverse)
+       getinverse = getinverse) # Returns the cachematrix
 
 }
 
 
 ## Write a short comment describing this function
+# Input: CacheMatrix
+# Output: The inverse of the cachematrix
+# Calculates and stores in the cache if not already in the cache
+# Otherwise retrieves from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -29,6 +38,7 @@ cacheSolve <- function(x, ...) {
     print("Computing")
     x$setinverse(solve(x$get()))
   }
+  # Else just gets the inverse from the cache
   else{ print("getting from cache")}
   return(x$getinverse())
 }
