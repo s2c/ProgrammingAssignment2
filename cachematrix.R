6bf412454f9b46e0ff2b6f2ffd95b79a1f0f497a
Varun Somani <varunsomani95@gmail.com>
2018-09-09 06:14:27 +0530

    Committing final

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -12,4 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  # if the inverse is null then it computes the inverse and stores it into x
+  if (is.null(inv)){
+    print("Computing")
+    x$setinverse(solve(x$get()))
+  }
+  else{ print("getting from cache")}
+  return(x$getinverse())
 }
